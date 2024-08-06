556e555eb0c86d4b9eccacad75a7ef852c0a3e1e
Imanm02 <imanm1381@gmail.com>
2024-08-06 19:07:53 +0330

    Update Pic Choose Action

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
   const grid = document.querySelector('.grid');
+  const captions = [
+    "Here is our first picture.",
+    "Here is our second picture.",
+    "Here is our third picture.",
+    
+  ];
+
   for (let i = 1; i <= 63; i++) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
     cell.style.backgroundImage = `url('images/grids_${String(i).padStart(2, '0')}.jpg')`;
     cell.onclick = function() {
-      showModal(i);
+      showModal(i, captions[i-1]);
     };
     grid.appendChild(cell);
   }
 });
 
-function showModal(index) {
+function showModal(index, caption) {
   const modal = document.getElementById('modal');
   const modalImg = document.getElementById('img01');
   const captionText = document.getElementById('caption');
   modal.style.display = "block";
   modalImg.src = `images/grids_${String(index).padStart(2, '0')}.jpg`;
-  captionText.innerHTML = `توضیحات برای قطعه ${index}`;
+  captionText.innerHTML = caption;
   document.querySelector('.close').onclick = function() {
     modal.style.display = "none";
   };
