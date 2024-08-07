04778cc77d3cf50bf09ea9ff2c538dd4308f0c6e
Imanm02 <imanm1381@gmail.com>
2024-08-08 00:52:52 +0330

    Update captions css

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ document.addEventListener('DOMContentLoaded', function() {
   const cssFile = document.querySelector('link[rel="stylesheet"]');
   cssFile.href += '?v=' + new Date().getTime();
 
+  const captions = [
+    "توضیح تصویر اول",
+    "توضیح تصویر دوم",
+    "توضیح تصویر سوم",
+  ];
+
   const grid = document.querySelector('.grid');
   const modal = document.getElementById('modal');
   const modalImg = document.getElementById('img01');
@@ -10,9 +16,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
   grid.addEventListener('click', function(event) {
     if (event.target.classList.contains('cell')) {
+      const index = Array.from(grid.children).indexOf(event.target);
       modal.style.display = 'flex';
       modalImg.src = event.target.style.backgroundImage.slice(5, -2);
-      captionText.textContent = 'توضیحات تصویر اینجا';
+      captionText.textContent = captions[index];
     }
   });
 
